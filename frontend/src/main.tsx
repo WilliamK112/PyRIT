77cbc4c44c2e0a3f910db358adb72552df02354e
Ching Wei Kang <164879897+WilliamK112@users.noreply.github.com>
2026-09-13 18:48:30 -0500

    REFACTOR mount app router only after auth initialization

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import App from './App'
+
+import AppRouter from './AppRouter'
 import { AuthProvider } from './auth/AuthProvider'
 import { ThemeProvider } from './hooks/useTheme'
 import './styles/global.css'
-
-function AppRouter() {
-  const [router] = React.useState(() => createBrowserRouter([
-    {
-      path: '*',
-      element: <App />,
-    },
-  ]))
-
-  return <RouterProvider router={router} />
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
